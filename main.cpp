737e6bbd1b38749693d4d0e3674ceeec7e66d620
Ej19748 <ej19748@email.vccs.edu>
2024-09-12 00:11:58 +0000

    Added int and float code
    Wrote code to display age and weight

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 // Elias Jackson
 // Date: 9/11/2024
 // Chapter 2
-// Checkpoint 2.14
+// Checkpoint 2.20
 #include <iostream>
 using namespace std;
 int main() {
-  char first, middle, last;
-  first = 'E';
-  middle = 'O';
-  last = 'J';
-  cout << first << "" << middle << "" << last << endl;
+  int age;
+  float weight;
+  age = 18;
+    weight = 170;
+  cout << "My age is" << age << endl;
+  cout << "My weight is" << weight << endl;
 return 0;}
