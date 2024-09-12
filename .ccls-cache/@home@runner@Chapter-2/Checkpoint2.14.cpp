47e0b449b18976e2027192a591a4604980f9eb10
Ej19748 <ej19748@email.vccs.edu>
2024-09-12 00:00:38 +0000

    Added string code
    Wrote code to display name address and phone number

---
+++ .ccls-cache/@home@runner@Chapter-2/Checkpoint2.14.cpp
@@ -0,0 +1,12 @@
+// Elias Jackson
+// Date: 9/11/2024
+// Chapter 2 exercises
+// Checkpoint 2.17
+#include <iostream>
+using namespace std;
+int main() {
+  string name = "Elias Jackson";
+  string address = "123 playground street";
+  string phone = "123-456-7890";
+  cout << name << endl << address << endl << phone << endl << endl;
+return 0;}
